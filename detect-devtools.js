13aa48e8aa1d9bffc3e59fd9d78438fd7a3c8408
Omri Lotan <omri@hamadgera.com>
2016-12-24 20:14:52 +0200

    tidy up

--- detect-devtools.js
+++ detect-devtools.js
@@ -1,53 +1,50 @@
 (function (window) {
-    var debounce = function debounce (fn, delay) {
-        var timer = 0,
-            doit = function _doit () {
-                timer = 0;
-                fn();
-            },
-            later = function _later () {
-                if (timer) {
-                    clearTimeout(timer);
-                }
-                timer = setTimeout(doit, (delay || 200));
-            };
-        return later;
-    };
-
-    var detectDevTools = function () {
-
-            var conditions = [
-
-                    // width diff
-                    (window.outerWidth - window.innerWidth > 150),
-
-                    // height diff
-                    (window.outerHeight - window.innerHeight > 150),
-
-                    // Firebug initialized
-                    (window.Firebug && window.Firebug.chrome && window.Firebug.chrome.isInitialized)
-                ],
-                before = window.devtools;
-
-            if (conditions.indexOf(true) !== -1) {
-
-                // We assume dev tools are open
-                window.devtools = true;
-            } else {
-                window.devtools = false;
-            }
-
-            // Only if status has changed, trigger the event
-            if (window.devtools.open !== before) {
-                window.dispatchEvent(new CustomEvent("devtools", { detail: { devtools: window.devtools.open } }));
-            }
-        };
-
-    window.devtools = false;
-
-    // Detect
-    detectDevTools();
+    var devtools;
 
     // Update each window resize event
     window.addEventListener('resize', debounce(detectDevTools));
+
+    // Allow to trigger 'devtools' event
+    window.addEventListener('trigger-devtools', debounce(detectDevTools));
+
+    function detectDevTools (e) {
+        var conditions = [
+
+                // width diff
+                (window.outerWidth - window.innerWidth > 150),
+
+                // height diff
+                (window.outerHeight - window.innerHeight > 150),
+
+                // Firebug initialized
+                (window.Firebug && window.Firebug.chrome && window.Firebug.chrome.isInitialized)
+            ],
+            before = devtools;
+
+        // We only assume whether dev tools are open
+        devtools = conditions.indexOf(true) !== -1;
+
+        // Only if status has changed, trigger the event
+        if (devtools !== before) {
+            window.dispatchEvent(new CustomEvent("devtools", { detail: { devtools: devtools } }));
+        }
+    };
 }(window));
+
+function debounce(subscriber, delay = 200) {
+    let timer = 0;
+
+    function debounced() {
+        clearTimeout(timer);
+        let args = arguments;
+        [].unshift.call(args, this);
+
+        timer = setTimeout(Function.prototype.bind.apply(register, args), delay);
+    }
+
+    function register() {
+        timer = 0;
+        subscriber.apply(this, arguments);
+    }
+    return debounced;
+};
