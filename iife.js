ab810bb76ca02c7695828cf6d0147a9621836920
Omri Lotan <omrilotan@users.noreply.github.com>
2019-06-02 22:39:31 +0300

    same old same old

---
+++ iife.js
@@ -0,0 +1,2 @@
+import { warn } from './lib/warn/index.js';
+warn();
---
+++ index.js
@@ -0,0 +1,3 @@
+export { devtoolsOpen } from './lib/devtoolsOpen/index.js';
+export { debounce } from './lib/debounce/index.js';
+export { warn } from './lib/warn/index.js';
